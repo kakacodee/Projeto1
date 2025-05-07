f1f53dbcfbb38c0853b96a01b7e0fcd5adb1fbb5
Kauã <kakas13.souza@gmail.com>
2025-05-07 08:44:26 -0300

    criando uma requisicao post

--- Projeto1/Controllers/UsuarioController.cs
+++ Projeto1/Controllers/UsuarioController.cs
@@ -7,14 +7,28 @@ namespace Projeto1.Controllers
     {
         //Declarando uma variável privada somente para leitura do tipo do UsuarioRepositorio chamada _usuarioRepositorio
         private readonly UsuarioRepositorio _usuarioRepositorio;
+        /*Definindo o construtor parar a classe UsuarioController que vai receber uma
+         instância de UsuarioRepositorio */
+        public UsuarioController(UsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+            
+        }
         public IActionResult Login()
         {   
 
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string email, string senha)
+        {
             return View();
         }
         public IActionResult Contato()
         {
             return View();
         }
+     
     }
 }
